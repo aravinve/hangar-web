147a4ae40319144a9d003350c2cd026bb27e02bf
Aravind Venkatesan <aravindtherocker@gmail.com>
2020-11-28 23:36:33 +0800

    Remove Redundant Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="p-10 text-3xl text-red-600 dark:text-gray-900">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10 text-3xl text-white bg-black dark:bg-white text-black">
+      <h1 className="text-5xl">Hangar</h1>
+      <p className="text-sm">All Your Apps In One Place</p>
     </div>
   );
 }
